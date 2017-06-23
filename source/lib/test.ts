fcda6e790b774911b92cc983f8f41e97f1951466
stoiet <to.stoiet@gmail.com>
2017-06-23 13:23:46 +0200

    chore(tape): remove

--- source/lib/test.ts
+++
@@ -1,8 +0,0 @@
-import * as test from 'tape';
-
-export const singleTest = (description: string, fn: (t: test.Test) => void) => {
-  test(description, (t: test.Test) => {
-    fn(t);
-    t.end();
-  });
-};
