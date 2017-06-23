904bd9c4b1b2a2f078c2394581107f377e3a6814
stoiet <to.stoiet@gmail.com>
2017-06-23 13:22:13 +0200

    test(jasmine): setup

--- source/collectors/font-face-mixin.spec.ts
+++ source/collectors/font-face-mixin.spec.ts
@@ -1,70 +1,79 @@
-import * as test from 'tape';
-import { singleTest } from '../lib/test';
 import { collectFromFontFaces } from './font-face-mixin';
 
-singleTest('should give back empty array if there is no font in the given input', (t: test.Test) => {
-  const html = '<div>What</div>';
-  t.deepEqual(collectFromFontFaces(html), []);
+describe('#collectFromFontFaces', () => {
+
+  it('should give back empty array if there is no font in the given input', () => {
+    const html = '<div>What</div>';
+    expect(collectFromFontFaces(html)).toEqual([]);
+  });
+
+
+  it('should give back font family from font-face', () => {
+    const html = `
+      @font-face {
+        font-family:'Vardono';
+      }
+    `;
+    expect(collectFromFontFaces(html)).toEqual(['Vardono']);
+  });
+
+
+  it('should not give back font family if its not in font-face mixin', () => {
+    const html = `
+      body: {
+        font-family:Roboto;
+      }
+      h1: {
+        font-family:Verdana;
+      }
+    `;
+    expect(collectFromFontFaces(html)).toEqual([]);
+  });
+
+
+  it('should give back only font families', () => {
+    const html = `
+      @font-face {
+        font-color: red;
+        font-family:Arial;
+        font-style: normal;
+      }
+    `;
+    expect(collectFromFontFaces(html)).toEqual(['Arial']);
+  });
+
+
+  it('should ignore whitespaces, and ticks around font families', () => {
+    const html = `
+      @font-face {
+        font-family: 'Montserrat';
+      }
+    `;
+    expect(collectFromFontFaces(html)).toEqual(['Montserrat']);
+  });
+
+
+  it('should works with mustache too', () => {
+    const html = `
+      @font-face {
+        font-family: "Montserrat";
+      }
+    `;
+    expect(collectFromFontFaces(html)).toEqual(['Montserrat']);
+  });
+
+
+  it('should collect every font family', () => {
+    const html = `
+      @font-face {
+        font-family: 'Montserrat';
+      }
+      @font-face {
+        font-family: 'Wharane';
+      }
+    `;
+    expect(collectFromFontFaces(html)).toEqual(['Montserrat', 'Wharane']);
+  });
+
 });
 
-singleTest('should give back font family from font-face', (t: test.Test) => {
-  const html = `
-    @font-face {
-      font-family:'Vardono';
-    }
-  `;
-  t.deepEqual(collectFromFontFaces(html), ['Vardono']);
-});
-
-singleTest('should not give back font family if its not in font-face mixin', (t: test.Test) => {
-  const html = `
-    body: {
-      font-family:Roboto;
-    }
-    h1: {
-      font-family:Verdana;
-    }
-  `;
-  t.deepEqual(collectFromFontFaces(html), []);
-});
-
-singleTest('should give back only font families', (t: test.Test) => {
-  const html = `
-    @font-face {
-      font-color: red;
-      font-family:Arial;
-      font-style: normal;
-    }
-  `;
-  t.deepEqual(collectFromFontFaces(html), ['Arial']);
-});
-
-singleTest('should ignore whitespaces, and ticks around font families', (t: test.Test) => {
-  const html = `
-    @font-face {
-      font-family: 'Montserrat';
-    }
-  `;
-  t.deepEqual(collectFromFontFaces(html), ['Montserrat']);
-});
-
-singleTest('should works with mustache too', (t: test.Test) => {
-  const html = `
-    @font-face {
-      font-family: "Montserrat";
-    }
-  `;
-  t.deepEqual(collectFromFontFaces(html), ['Montserrat']);
-});
-
-singleTest('should collect every font family', (t: test.Test) => {
-  const html = `
-    @font-face {
-      font-family: 'Montserrat';
-    }
-    @font-face {
-      font-family: 'Wharane';
-    }
-  `;
-  t.deepEqual(collectFromFontFaces(html), ['Montserrat', 'Wharane']);
-});
--- source/collectors/google-font-link.spec.ts
+++ source/collectors/google-font-link.spec.ts
@@ -1,67 +1,76 @@
-import * as test from 'tape';
-import { singleTest } from '../lib/test';
 import { collectFromGoogleFontLinks } from './';
 
-singleTest('should give back empty array if there is no custom font in the given email', (t: test.Test) => {
-  const html = `
-    <html>
-      <body>
-        <div>Yo!</div>
-      </body>
-    </html>
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html), []);
-});
+describe('#collectFontFamilies', () => {
 
-singleTest('should find the given font family from link tag', (t: test.Test) => {
-  const html = `
-    <head>
+  it('should give back empty array if there is no custom font in the given email', () => {
+    const html = `
+      <html>
+        <body>
+          <div>Yo!</div>
+        </body>
+      </html>
+    `;
+    expect(collectFromGoogleFontLinks(html)).toEqual([]);
+  });
+
+
+  it('should find the given font family from link tag', () => {
+    const html = `
+      <head>
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+      </head>
+    `;
+    expect(collectFromGoogleFontLinks(html).length).toEqual(1);
+  });
+
+
+  it('should not find non custom font css link', () => {
+    const html = `
+      <link href="http://foobar.com/css?family=red" rel="stylesheet" type="text/css">
+    `;
+    expect(collectFromGoogleFontLinks(html).length).toEqual(0);
+  });
+
+
+  it('should give back the custom font name', () => {
+    const html = `
+      <link href="http://fonts.googleapis.com/css?family=Open" rel="stylesheet" type="text/css">
+    `;
+    expect(collectFromGoogleFontLinks(html)).toEqual(['Open']);
+  });
+
+
+  it('should give back the custom font name with normalized name', () => {
+    const html = `
       <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-    </head>
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html).length, 1);
-});
+    `;
+    expect(collectFromGoogleFontLinks(html)).toEqual(['Open Sans']);
+  });
 
-singleTest('should not find non custom font css link', (t: test.Test) => {
-  const html = `
-    <link href="http://foobar.com/css?family=red" rel="stylesheet" type="text/css">
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html).length, 0);
-});
 
-singleTest('should give back the custom font name', (t: test.Test) => {
-  const html = `
-    <link href="http://fonts.googleapis.com/css?family=Open" rel="stylesheet" type="text/css">
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html), ['Open']);
-});
+  it('should give back the custom font name when size and language options are added', () => {
+    const html = `
+      <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&amp;subset=greek" rel="stylesheet">
+    `;
+    expect(collectFromGoogleFontLinks(html)).toEqual(['Open Sans']);
+  });
 
-singleTest('should give back the custom font name with normalized name', (t: test.Test) => {
-  const html = `
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html), ['Open Sans']);
-});
 
-singleTest('should give back the custom font name when size and language options are added', (t: test.Test) => {
-  const html = `
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&amp;subset=greek" rel="stylesheet">
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html), ['Open Sans']);
-});
+  it('should find font family if only language options are given', () => {
+    const html = `
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&amp;subset=greek,latin-ext" rel="stylesheet">
+    `;
+    expect(collectFromGoogleFontLinks(html)).toEqual(['Open Sans']);
+  });
 
-singleTest('should find font family if only language options are given', (t: test.Test) => {
-  const html = `
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&amp;subset=greek,latin-ext" rel="stylesheet">
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html), ['Open Sans']);
-});
 
-singleTest('should work for more than one font family link', (t: test.Test) => {
-  const html = `
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&amp;subset=greek,latin-ext" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&amp;subset=latin-ext" rel="stylesheet">
-    <link href="http://foobar.com/css?family=red" rel="stylesheet" type="text/css">
-  `;
-  t.deepEqual(collectFromGoogleFontLinks(html), ['Open Sans', 'Source Sans Pro']);
+  it('should work for more than one font family link', () => {
+    const html = `
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&amp;subset=greek,latin-ext" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&amp;subset=latin-ext" rel="stylesheet">
+      <link href="http://foobar.com/css?family=red" rel="stylesheet" type="text/css">
+    `;
+    expect(collectFromGoogleFontLinks(html)).toEqual(['Open Sans', 'Source Sans Pro']);
+  });
+
 });
