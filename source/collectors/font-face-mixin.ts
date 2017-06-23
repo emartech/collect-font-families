ad7c74ec6148f311b5c3e6936e878b306b3df61e
stoiet <to.stoiet@gmail.com>
2017-06-23 11:48:33 +0200

    fix(match-all): add interface for module

--- source/collectors/font-face-mixin.ts
+++ source/collectors/font-face-mixin.ts
@@ -1,4 +1,4 @@
-import matchAll from 'match-all';
+import * as matchAll from 'match-all';
 const fontStyleValuesRegexp = /@font-face[\s]*{[\s\w:;-]*font-family:['"\s]*([\s\w-,]*)[;"']/gm;
 
 export const collectFromFontFaces = (html: string) => matchAll(html, fontStyleValuesRegexp).toArray();
---
+++ source/interface/match-all.ts
@@ -0,0 +1,5 @@
+
+declare module 'match-all' {
+    const matchAll: any;
+    export = matchAll;
+}
